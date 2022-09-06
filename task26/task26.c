7c25466cba575beb9abaadf748f08b22f0becbf4
Alex <a.gemuev@g.nsu.ru>
2022-09-06 20:54:53 +0700

    fix

--- task26/task26.c
+++ task26/task26.c
@@ -28,6 +28,7 @@ typedef struct ProducerContext producer_context_t;
 struct ConsumerContext{
     bq_charptr_t_t* bq;
     pthread_mutex_t* mutex;
+    int consumer_id;
 };
 typedef struct ConsumerContext consumer_context_t;
 
@@ -40,37 +41,45 @@ producer(void* arg){
         bq_charptr_t_put(context->bq, &(context->text));
 
         pthread_mutex_lock(context->mutex);
-        printf("[%s] put to queue\n", context->text);
+        printf(" ++++++ %s put to queue\n", context->text);
         pthread_mutex_unlock(context->mutex);
+        usleep( 500000 + 1000 * 100 * rand()%20);
     }
 
     return NULL;
 }
 
 
+enum {
+    COUNT_OF_PRODUCERS = 5,
+    COUNT_OF_PRODUCTS = 5,
+    COUNT_OF_CONSUMERS = 5
+};
+
+
 void*
 consumer(void* arg){
     consumer_context_t* context = (consumer_context_t*) arg;
 
-    while(1){
+    for(int i=0; i<COUNT_OF_CONSUMERS; ++i){
         charptr_t ptr;
         bq_charptr_t_take(context->bq, &ptr);
 
         pthread_mutex_lock(context->mutex);
-        printf("[%s] get from queue\n", ptr);
+        printf(" ------ %s get by consumer %d\n", ptr, context->consumer_id);
         pthread_mutex_unlock(context->mutex);
-        sleep(1);
+        usleep( 1000000 + 1000 * 100 * rand()%40);
     }
 
-    //return NULL;
+    return NULL;
 }
 
 
 int 
 main(){
 
-    pthread_t producers_threads[2];
-    pthread_t consumers[2];
+    pthread_t producers_threads[COUNT_OF_PRODUCERS];
+    pthread_t consumers_threads[COUNT_OF_CONSUMERS];
     pthread_attr_t thread_attr;
     pthread_attr_init(&thread_attr);
     pthread_attr_setstacksize(&thread_attr, PTHREAD_STACK_MIN);
@@ -81,22 +90,44 @@ main(){
     }
 
     pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-    producer_context_t prod_context1 = {&bq, "--------", 1000, &mutex};
-    producer_context_t prod_context2 = {&bq, "++++++++", 1000, &mutex};
-    consumer_context_t cons_context = {&bq, &mutex};
 
+    producer_context_t producers_context[COUNT_OF_PRODUCERS];
+    for(int i=0; i<COUNT_OF_PRODUCERS; ++i){
+        producers_context[i].bq = &bq;
+        producers_context[i].count = COUNT_OF_PRODUCTS;
+        producers_context[i].mutex = &mutex;
+        producers_context[i].text = malloc(10);
+        sprintf(producers_context[i].text, "%d", i);
 
-    if(pthread_create(&producers_threads[0], &thread_attr, producer, (void*) &prod_context1)==-1 ||
-        pthread_create(&producers_threads[1], &thread_attr, producer, (void*) &prod_context2)==-1 ||
-        pthread_create(&consumers[0], &thread_attr, consumer, (void*) &cons_context)==-1 ){
-
-        perror("Creating threads fail");
-        return 0;
+        if(pthread_create(&producers_threads[i], &thread_attr, producer, (void*) &producers_context[i])==-1){
+            perror("creation producer thread fail");
+            exit(0);
+        }
     }
 
-    pthread_join(producers_threads[0], NULL);
-    pthread_join(producers_threads[1], NULL); 
-    //pthread_join(thr);
+    consumer_context_t consumers_context[COUNT_OF_CONSUMERS];
+    for(int i=0; i<COUNT_OF_CONSUMERS; ++i){
+        consumers_context[i].bq = &bq;
+        consumers_context[i].mutex = &mutex;
+        consumers_context[i].consumer_id = i;
+
+        if(pthread_create(&consumers_threads[i], &thread_attr, consumer, (void*) &consumers_context[i])==-1){
+            perror("creation cunsumer thread fail");
+            exit(0);
+        }
+    }
+
+    for(int i=0; i<COUNT_OF_PRODUCERS; ++i){
+        pthread_join(producers_threads[i], NULL);
+    }
+
+    for(int i=0; i<COUNT_OF_CONSUMERS; ++i){
+        pthread_join(consumers_threads[i], NULL);
+    }    
+
+    for(int i=0; i<COUNT_OF_PRODUCERS; ++i){
+        free(producers_context[i].text);
+    }
     bq_charptr_t_destroy(&bq);
 
 	return 0;
